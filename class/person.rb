43e0039573b967c35aa1f3b8fb74c057a4c0f09b
d4nielj <d4niel.djm@gmail.com>
2021-10-04 12:23:50 -0500

    Add Teacher and Student classes

--- class/person.rb
+++ class/person.rb
@@ -1,11 +1,18 @@
 class Person
-  def initialize(name = "unknown", age, parent_permission = true)
+  def initialize(age, name = 'unknown', parent_permission: true)
     @name = name
     @age = age
-    @parent_permission = parent_permission      
+    @parent_permission = parent_permission
   end
-  
+
   attr_reader :id
-  attr_accessor :name
-  attr_accessor :age
+  attr_accessor :name, :age
+
+  def of_age?
+    @age >= 18
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
 end
