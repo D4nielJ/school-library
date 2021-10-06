915c02cf4b6d954cb6b1be7c5daed7bfb02319ec
d4nielj <d4niel.djm@gmail.com>
2021-10-06 13:51:00 -0500

    Add associations in books - rentals - person

--- class/student.rb
+++ class/student.rb
@@ -3,15 +3,15 @@ require_relative 'person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(*args, classroom)
+  def initialize(*args, classroom: nil)
     super(*args)
     @classroom = classroom
-    classroom.add_student(self) unless classroom.students.include(self)
+    classroom.add_student(self) if classroom && !classroom.students.include(self)
   end
 
   def classroom=(classroom)
     @classroom = classroom
-    classroom.add_student(self) unless classroom.students.include(self)
+    classroom.add_student(self) if classroom && !classroom.students.include(self)
   end
 
   def play_hookey
--- index.rb
+++ index.rb
@@ -1,8 +1,6 @@
-require_relative 'class/person'
-require_relative 'class/student'
-require_relative 'class/teacher'
+Dir[File.join(__dir__, 'class', '*.rb')].each { |file| require_relative file }
 
-daniel = Student.new(27, 'BUSARAKHAMANRAS', '11-C')
+daniel = Student.new(27, 'BUSARAKHAMANRAS')
 
 p daniel
 
