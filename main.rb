fec8b3f411b4039f888d2e1bfdc0fae6a2b931cc
d4nielj <d4niel.djm@gmail.com>
2021-10-06 20:49:33 -0500

    Refactor work to implement state in app

--- main.rb
+++ main.rb
@@ -1,30 +1,17 @@
 require_relative 'ui/ui_methods'
+
 class App
-  def initialize(ui_methods)
-    @ui = UserInterface.new
-    @ui_methods = ui_methods
+  def initialize(initialState)
+    @state = initialState
   end
 
   def init
-    puts 'Please choose an option by enter in a number =>'
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all rentals for a given person'
-    puts '7 - Exit'
-    choice_number = gets.chomp
-    execute(choice_number)
-  end
-
-  def execute(number)
-    @ui.ui_map(number)
+    
   end
 end
 
 def main
-  app = App.new()
+  app = App.new
   app.init
 end
 
