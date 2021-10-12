337e6532dd6387e2baa719b90c052e0d86317958
d4nielj <d4niel.djm@gmail.com>
2021-10-12 15:16:09 -0500

    Add method to create_files

--- app/file_manager.rb
+++ app/file_manager.rb
@@ -1,22 +1,34 @@
+require 'fileutils'
+require 'json'
+
 class FileManager
+  def initialize
+    @adress = './app/data'
+  end
+
   def fetch
-    initial_state = {
+    {
       books: [],
       people: [],
       rentals: [],
       exit: false
     }
-    
+
     ## Load files
 
     ## initial_state << books_array if books.json
     ## initial_state << people_array if books.json
     ## initial_state << rentals_array if books.json
-    
-    initial_state
   end
 
-  def save(state)
-    ## Write to files
+  def save(_state)
+    create_files
+  end
+
+  def create_files
+    Dir.mkdir(@adress) unless Dir.exist?(@adress)
+    FileUtils.touch("#{@adress}/books.json") unless File.exist?("#{@adress}/books.json")
+    FileUtils.touch("#{@adress}/people.json") unless File.exist?("#{@adress}/people.json")
+    FileUtils.touch("#{@adress}/rentals.json") unless File.exist?("#{@adress}/rentals.json")
   end
 end
