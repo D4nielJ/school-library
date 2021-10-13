f654659d0f4aae65cf7d32a85c1bf91c3fc2ec10
d4nielj <d4niel.djm@gmail.com>
2021-10-13 17:16:53 -0500

    Add tests for Student class

--- app/spec/student_spec.rb
+++ app/spec/student_spec.rb
@@ -1,1 +1,16 @@
-## Daniel will do these tests
+require_relative '../class/index'
+
+describe Student do
+  it 'create a new Student with correct attributes and methods' do
+    student = Student.new(name: 'John Doe', age: 17, parent_permission: false)
+
+    expect(student).to have_attributes(name: 'John doe')
+    expect(student).to have_attributes(age: 17)
+    expect(student).to have_attributes(parent_permission: false)
+    expect(student).to have_attributes(rentals: [])
+    expect(student).to have_attributes(classroom: nil)
+    expect(student.id).to be_between(1, 100_000).inclusive
+    expect(student.of_age?).to eql false
+    expect(student.can_use_services?).to eql false
+  end
+end
