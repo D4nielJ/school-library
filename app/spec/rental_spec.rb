128b579d964ae24efef228590ba5948711c05c10
d4nielj <d4niel.djm@gmail.com>
2021-10-14 10:53:20 -0500

    Fix rental tests

--- app/spec/rental_spec.rb
+++ app/spec/rental_spec.rb
@@ -2,9 +2,16 @@ Dir[File.join(__dir__, '../class', '*.rb')].each { |file| require_relative file 
 
 describe Rental do
   context 'With valid input' do
-    it 'should create a rental with person Richie Rod' do
-      per = Rental.new(date: 'November 11, 2021', person: 'Richie Rod', book: 'The way')
-      expect(per.date == 'November 11, 2021').to be true
+    it 'should create a rental with person and book objects' do
+      person = Person.new(name: 'John Doe', age: 17, parent_permission: false)
+      book = Book.new(title: 'testName', author: 'testAuthor')
+
+      rental = Rental.new(date: 'November 11, 2021', person: person, book: book)
+      expect(rental.date == 'November 11, 2021').to be true
+      expect(rental.book).to eql book
+      expect(rental.person).to eql person
+      expect(person.rentals).to eql [rental]
+      expect(book.rentals).to eql [rental]
     end
   end
 end
